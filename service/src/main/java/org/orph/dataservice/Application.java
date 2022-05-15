6206ed784c296e9d61b8c554253e338113770570
Maphsge7 <73021655+Maphsge7@users.noreply.github.com>
2022-05-15 22:48:13 +0800

    nacos success

--- service/src/main/java/org/orph/dataservice/Application.java
+++ service/src/main/java/org/orph/dataservice/Application.java
@@ -3,9 +3,12 @@ package org.orph.dataservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import com.alibaba.nacos.api.config.annotation.NacosValue;
+//import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 
 @MapperScan("org.orph.dataservice.mapper")
 @SpringBootApplication
+//@NacosPropertySource(dataId = "bamboo.test", autoRefreshed = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
